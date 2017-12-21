b71338eda886da28c9454034b253264406a3434b
Alex Holachek <aholachek@gmail.com>
2017-12-21 09:48:04 -0500

    hmm

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import generateReduxReport from './generateReduxReport'
-import saveReport from './saveReport.js'
 import trackObjectUse from './trackObjectUse'
 
 export default {
   generateReduxReport,
-  saveReport,
   trackObjectUse
 }
