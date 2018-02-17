b83f56ccc04bd4194476fa19f563a98a7727b99b
Alex Holachek <aholachek@gmail.com>
2018-02-16 20:42:01 -0500

    trying to figure out this react-json-tree dependency issue

--- src/monitor/ReduxTree.js
+++ src/monitor/ReduxTree.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react"
 import PropTypes from "prop-types"
-import JSONTree from "react-json-tree"
+import JSONTree from "./react-json-tree-modified"
 import styled from "styled-components"
 
 const FadeSpan = styled.span`
